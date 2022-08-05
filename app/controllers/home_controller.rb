9c857d9a79d6d1ae06f920c131e20b3f9a905b46
get.dipen@gmail.com <get.dipen@gmail.com>
2022-08-05 11:12:28 -0700

    Added home controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/about'
+  get 'home/portfolio'
+  get 'home/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
