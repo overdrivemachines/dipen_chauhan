0b24baa7b9f3a7faeadd4c1beea75a8d288afb08
get.dipen@gmail.com <get.dipen@gmail.com>
2022-08-05 11:32:47 -0700

    added bootstrap, font awesome, jquery, custom js and css

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,14 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+# From "jquery-rails" gem
+pin "jquery", to: "jquery3.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+
+# From "bootstrap" gem
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
+
+# Use all.js instead of fontawesome.js
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@6.1.2/js/all.js"
