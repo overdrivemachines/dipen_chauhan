a9bae65e620d73c188859d611cf1f4bb1e4e9d6b
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-24 02:19:22 -0800

    dynamically add active css class

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Returns active if the specified path is the current path
+  # Used in the navbar partial to add active class to icons
+  def active?(path)
+    return "active" if request.path == path
+
+    ''
+  end
 end
