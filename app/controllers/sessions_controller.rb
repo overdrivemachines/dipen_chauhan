7ee95febd0cb53388604ddb4dd21b0646264617d
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-23 20:47:04 -0800

    send email that contains login link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,22 @@ class SessionsController < ApplicationController
   def new; end
 
   # Login form has posted
-  def create; end
+  def create
+    # Find the user based on their email provided in the login form
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    # if user exists send the login email
+    if user
+      # Send email to user
+      UserMailer.account_login(user)
+    end
+
+    # Redirect to page stating that email was sent
+    redirect_to message_sessions_path,
+                notice: "If an account for your email exists, the login link has been emailed to you."
+  end
+
+  def message; end
 
   # Logout
   def destroy; end
