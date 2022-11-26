4d4f424737e757193303641f846ef3848fa9ecbc
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-25 19:06:56 -0800

    create new user with "login"

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: "get.dipen@gmail.com", first_name: "Dipen", last_name: "Chauhan")
+User.create(email: "get.dipen@gmail.com", first_name: "Dipen", last_name: "Chauhan", login: "helloworld")
