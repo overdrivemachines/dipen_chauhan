962a583ef6e9a7eebfbd12786e3cfb10daa0551e
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-25 19:06:04 -0800

    added edit action to login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   # GET /login
   def new; end
 
-  # Login form has posted
+  # Login form has submitted/posted
   # POST /login
   def create
     # Find the user based on their email provided in the login form
@@ -19,6 +19,25 @@ class SessionsController < ApplicationController
     flash[:title] = "Notice"
     redirect_to sessions_message_path,
                 notice: "If an account for your email exists, the login link has been emailed to you."
+  end
+
+  # Edit action gets called when user clicks on activation link in email
+  # e.g. link: http://localhost:3000/sessions/ChCkiCMDIodAi6uG8-aYEw/edit?email=c%40c.com
+  # Has no template
+  def edit
+    user = User.find_by(email: params[:email])
+    login_token = params[:id]
+
+    if user && user.authenticate_login(login_token)
+      # Check for expiry
+      # TODO: login user
+      flash[:success] = "Account Logged In!"
+      redirect_to projects_path
+    else
+      flash[:title] = "Invalid Login"
+      redirect_to sessions_message_path,
+                  notice: "Login link is invalid. Please get a new link and try again. Login links expire after 2 hours."
+    end
   end
 
   # GET /sessions/message
