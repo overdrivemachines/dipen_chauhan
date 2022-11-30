1916c96d6b28f684f14d11ac2757a6fa130dfbf0
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-30 01:57:08 -0800

    finding current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,37 @@
 module SessionsHelper
   # Logs in the user:
   # - sets session[:user_id]
-  # - creates new remember_digest
-  # - sets session[:remember_digest]
-  # - resets user.login_digest
+  # - creates new login token
+  # - sets session[:login_token]
   def log_in(user)
-    # Places a temporary cookie on the user’s browser
+    # Place a temporary cookie on the user’s browser
     # containing an encrypted version of the user’s id.
     # Expires immediately when the browser is closed.
     session[:user_id] = user.id
 
-    # create new remember digest
-    user.remember_digest = User.digest(User.new_token)
+    # create new login token
+    token = User.new_token
+    user.login = token
 
-    # Guard against session replay attacks.
-    # See https://bit.ly/33UvK0w for more.
-    session[:remember_digest] = user.remember_digest
+    # TODO: reset login_sent_at
 
-    # make login link invalid by resetting the login_digest
-    user.login = User.new_token
+    # save new login token in session
+    session[:login_token] = token
+
+    # save user with new login token to database
     user.save
+  end
+
+  # Returns the current logged-in user (if any)
+  def current_user
+    # Check if session[:user_id] is present
+    return unless user_id == session[:user_id]
+
+    # get the user having id as user_id
+    user = User.find_by(id: user_id)
+
+    # if user and login_token combination is valid
+    @current_user = user if user&.authenticate_login(session[:login_token])
   end
 
   def log_out
