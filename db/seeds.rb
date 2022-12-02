0cc0709a7db6ea1b4a3f0befa3877c9e8e867e18
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-01 21:24:33 -0800

    added categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,6 @@
 
 User.create(email: "get.dipen@gmail.com", first_name: "Dipen", last_name: "Chauhan", login: "helloworld")
 
-Category.create(name: "Ruby on Rails", abbr: "ror");
-Category.create(name: "JavaScript", abbr: "js");
-Category.create(name: "C++", abbr: "cpp");
+Category.create(name: "Ruby on Rails", abbr: "ror")
+Category.create(name: "JavaScript", abbr: "js")
+Category.create(name: "C++", abbr: "cpp")
