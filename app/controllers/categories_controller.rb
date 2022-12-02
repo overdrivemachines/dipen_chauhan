ec98915278d9dcac90e79d8c911a627d03b7b4b1
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-02 13:36:37 -0800

    redirect to projects url for create, update and delete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,14 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
-
-  # GET /categories
-  def index
-    @categories = Category.all
-  end
-
-  # GET /categories/1
-  def show
-  end
+  before_action :set_category, only: %i[ edit update destroy ]
 
   # GET /categories/new
   def new
@@ -16,15 +7,14 @@ class CategoriesController < ApplicationController
   end
 
   # GET /categories/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /categories
   def create
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to @category, notice: "Category was successfully created."
+      redirect_to projects_url, notice: "Category was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +23,7 @@ class CategoriesController < ApplicationController
   # PATCH/PUT /categories/1
   def update
     if @category.update(category_params)
-      redirect_to @category, notice: "Category was successfully updated."
+      redirect_to projects_url, notice: "Category was successfully updated."
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +32,18 @@ class CategoriesController < ApplicationController
   # DELETE /categories/1
   def destroy
     @category.destroy
-    redirect_to categories_url, notice: "Category was successfully destroyed."
+    redirect_to projects_url, notice: "Category was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name, :abbr)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name, :abbr)
+  end
 end
