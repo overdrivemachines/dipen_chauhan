bc5e1e0f87677a7b108f87de3e46167f2a3155d7
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-03 21:41:13 -0800

    added before_destroy

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,4 +10,23 @@
 #
 class Category < ApplicationRecord
   has_many :projects
+  # Before a category is destroyed, all projects of this category
+  # need to be reassigned different categories
+  before_destroy :reassign_projects_category
+
+  private
+
+  # Callback called before destroy
+  # Do not destroy when only one category is left
+  # Do not destroy "Hidden" category
+  # when a category is destroyed, assign the foreign keys of
+  # Projects to the hidden category
+  def reassign_projects_category
+    return false if Category.count == 1
+    return false if abbr == "Hidden"
+
+    projects.each do |project|
+      project.category_id = Category.find_by(abbr: "Hidden").id
+    end
+  end
 end
