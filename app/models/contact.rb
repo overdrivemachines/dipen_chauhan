1ed38d5ec0f40359f02e1a381af6427cdef246a1
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-28 22:19:58 -0800

    added attributes and headers method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,15 @@
 class Contact < MailForm::Base
-  attribute :name
-  attribute :email
-  attribute :subject
-  attribute :message
+  attribute :name,    validate: true
+  attribute :email,   validate: /\A([\w.%+-]+)@([\w-]+\.)+(\w{2,})\z/i
+  attribute :subject, validate: true
+  attribute :message, validate: true
+  attribute :nickname, captcha: true
 
   def headers
-    { to: "PLEASE-CHANGE-ME@example.org" }
+    {
+      to: "get.dipen@gmail.com",
+      subject:,
+      from: %("#{name}" <#{email}>)
+    }
   end
 end
