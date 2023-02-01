94e7d1e57a48c5158134c14e0507f4503540962f
get.dipen@gmail.com <get.dipen@gmail.com>
2023-02-01 00:47:16 -0800

    added host to default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  host = 'dipenchauhan.com' # replace with your own url
-  config.action_mailer.default_url_options({ host: })
+  config.action_mailer.default_url_options({ host: "dipenchauhan.com"})
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
