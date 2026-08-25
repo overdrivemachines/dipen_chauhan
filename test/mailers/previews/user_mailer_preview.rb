cd5c244c7434bafe7489346636c6350e5103c294
Dipen Chauhan <get.dipen@gmail.com>
2026-08-25 01:12:38 -0500

    update account_login preview to include user object and login token

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,6 +2,8 @@
 class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/account_login
   def account_login
-    UserMailer.account_login
+    user = User.first || User.new(email: "preview@example.com")
+    user.login_token = "preview-login-token"
+    UserMailer.account_login(user)
   end
 end
